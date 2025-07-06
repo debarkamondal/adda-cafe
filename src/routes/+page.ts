2fb7cf6f33c30bdd375a10d6acfc348145b76a8a
dezire <devarkamondal@gmail.com>
2025-07-06 14:55:46 +0530

    making sure static adapter works with dynaic routes

--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: PageLoad = async ({ url }) => {
-	const token = url.searchParams.get('token');
-	if (!token) redirect(301, '/reserve');
-};
