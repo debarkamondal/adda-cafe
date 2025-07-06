d1eaea1b3a0e15147bd9fd6831057dff9c4bb1b6
dezire <devarkamondal@gmail.com>
2025-07-06 14:52:06 +0530

    making sure static adapter works with dynaic routes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('https://api.sampleapis.com/futurama/episodes');
-	const data = await res.json();
-	return { data };
+export const load: PageLoad = async ({ url }) => {
+	const token = url.searchParams.get('token');
+	if (!token) redirect(301, '/reserve');
 };
