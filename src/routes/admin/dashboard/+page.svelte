1c760ab8f4745cae108f114ed72c5376b88f14a4
dezire <devarkamondal@gmail.com>
2025-07-24 14:35:47 +0530

    prettified the output of pending acgitions

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -3,6 +3,7 @@
 	import Card from '$lib/components/Card.svelte';
 	import { browser } from '$app/environment';
 	import { PUBLIC_BACKEND_URL } from '$env/static/public';
+	import Dialog from '$lib/components/Dialog.svelte';
 
 	// let actions = $state([
 	// 	{
@@ -14,11 +15,16 @@
 	// 		description: 'description 2'
 	// 	}
 	// ]);
+	let dialogRef = $state<HTMLDialogElement>();
+	let transitionOpen = $state(false);
+	let reason = $state('');
 	let status = $state<'connecting' | 'connected' | 'loading' | 'loaded'>('connecting');
 	let actions = $state<
 		{
-			name: string;
-			description: string;
+			type: 'session';
+			id: string;
+			createdAt: number;
+			[key: string]: any;
 		}[]
 	>([]);
 
@@ -40,8 +46,7 @@
 			}
 			return false;
 		});
-		const url= PUBLIC_BACKEND_URL.replace('http','ws')+"/admin/ws"
-		console.log(url)
+		const url = PUBLIC_BACKEND_URL.replace('http', 'ws') + `/admin/ws?X-CSRF-TOKEN=${csrfToken}`;
 		const ws = new WebSocket(url);
 		ws.onopen = async () => {
 			status = 'connected';
@@ -49,13 +54,11 @@
 		};
 		ws.onmessage = async (event) => {
 			const data: Record<string, any> = JSON.parse(event.data);
-			delete data.pk;
-			delete data.sk;
-			console.log(data);
 			for (const action in data) {
 				actions.push({
-					name: action.split(':')[0],
-					description: data[action]
+					type: data[action].type,
+					id: data[action].id,
+					...data[action]
 				});
 			}
 		};
@@ -86,13 +89,17 @@
 			<h2 class="mb-4 text-center text-lg font-medium underline underline-offset-1">
 				Pending actions
 			</h2>
-			{#each actions as action, index (action.name)}
+			{#each actions as action: Record<string,any>, index (action.id)}
 				<div class="border-accent-300 mb-2 overflow-hidden rounded-lg border shadow-sm">
 					<button
 						onclick={() => handleActionExpansion(index)}
 						class="hover:bg-accent-800 focus:bg-accent-800 bg-primary-800 flex w-full items-center justify-between px-4 py-3 text-left transition-colors duration-200 focus:outline-none"
 					>
-						<span class="text-primary-100 font-medium capitalize">{action.name}</span>
+						{#if action.type === 'session'}
+							<span class="text-primary-100 font-medium capitalize"
+								>Reserved Table: {action['tableId']}</span
+							>
+						{/if}
 						<svg
 							class="text-accent-200 h-5 w-5 transform transition-transform duration-200 {actionOpened ===
 							index
@@ -111,10 +118,38 @@
 						</svg>
 					</button>
 					{#if actionOpened === index}
+						{@const { id, type, createdAt, ...keys } = action}
+						{@const date = new Date(createdAt)}
 						<div class="border-accent-200 bg-accent-50 border-t">
-							<p transition:slide={{ duration: 400 }} class="text-primary-900 p-4 leading-relaxed">
-								{JSON.stringify(action.description)}
-							</p>
+							<div
+								transition:slide={{ duration: 400 }}
+								class="text-primary-900 p-4 leading-relaxed"
+							>
+								{#each Object.keys(keys) as key}
+									<p>
+										<span class="font-semibold capitalize">{key}:</span>
+										<span class="capitalize">{keys[key]}</span>
+									</p>
+								{/each}
+								<p class="flex flex-col">
+									<span
+										><span class="mr-1 font-semibold">CreatedAt: </span>{date.toLocaleTimeString()}
+										({date.getDate()}/{date.getMonth()}/{date.getFullYear()})</span
+									>
+									<span><span class="mr-1 font-semibold">Id:</span>{id}</span>
+								</p>
+								<div class="text-accent-50 -mx-2 mt-2 flex justify-around gap-2">
+									<button
+										class="bg-accent-900 mx-auto h-12 rounded-md p-2 px-4"
+										onclick={() => {
+											dialogRef?.showModal();
+											transitionOpen = true;
+										}}><img alt="trash-icon" src="/icons/trash.svg" /></button
+									>
+									<button class="bg-accent-600 mx-auto h-12 grow rounded-md p-2 px-4">Accept</button
+									>
+								</div>
+							</div>
 						</div>
 					{/if}
 				</div>
@@ -122,3 +157,16 @@
 		{/if}
 	</Card>
 </section>
+<Dialog title="Revoke session?" bind:dialogRef bind:transitionOpen>
+	<p class="text-center text-lg font-semibold">Why do you want to revoke session?</p>
+	<form class="text-center">
+		<input
+			type="text"
+			id="reason"
+			bind:value={reason}
+			class="border-accent-300 bg-accent-50 text-primary-950 focus:border-accent-300 focus:ring-accent-300 my-2 w-full rounded-md border px-3 py-2 shadow-sm transition-colors focus:ring-2 focus:outline-none"
+			placeholder="Enter reason"
+		/>
+		<button class="bg-accent-600 mx-auto h-12 grow rounded-md p-2 px-4">Revoke Session</button>
+	</form>
+</Dialog>
